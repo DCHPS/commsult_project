2e574f8f91b43d1eb2a6b577a40dd05b3f66f8a9
Andre Wijaya <andrewijaya2911@gmail.com>
2018-10-05 15:27:20 +0700

    test push

---
+++ commsult_training_project/src/test/java/id/ac/sgu/commsult_training_project/junit_test.java
@@ -0,0 +1,15 @@
+package id.ac.sgu.commsult_training_project;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class junit_test {
+	
+	@Test
+	public void testJunit()
+	{
+		assertEquals(4,4);
+	}
+
+}
