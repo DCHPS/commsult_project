f015a18b6f9c007409e484928f7ab85dfa960f3e
Reischaga <reischaga@gmail.com>
2018-10-08 11:27:38 +0700

    commit1

--- src/main/java/id/ac/sgu/commsult_training_project/MainClass.java
+++ src/main/java/id/ac/sgu/commsult_training_project/MainClass.java
@@ -1,8 +1,11 @@
 package id.ac.sgu.commsult_training_project;
 import java.awt.*;
+import java.util.*;
 import javax.swing.*;
 
 public class MainClass extends Frame {
+	static JLabel temp = new JLabel();
+	JLabel blind = new JLabel();
 	
 	public MainClass(MainController mc) {
 		JFrame simpleGUI = new JFrame();
@@ -14,8 +17,8 @@ public class MainClass extends Frame {
 		
 		JPanel simplePanel = new JPanel();
 		
-		JLabel temp = new JLabel("The AC Temperature is: ");
-		JLabel blind = new JLabel("The Blinds are: ");
+		temp.setText("The AC Temperature is: "+mc.getAirConTemp());
+		blind.setText("The Blinds are up: "+mc.getBlindStatus());
 		
 		GridLayout myLayout = new GridLayout(2,0);
 		simplePanel.setLayout(myLayout);
@@ -26,18 +29,40 @@ public class MainClass extends Frame {
 		
 		simpleGUI.add(simplePanel);
 		simpleGUI.setVisible(true);
+		
+		
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		TimeSensor timeSensor = new TimeSensor(30);
+		boolean thereisNoGod = true;
+		TimeSensor timeSensor = new TimeSensor();
 		TemperatureSensor tempSensor = new TemperatureSensor(30);
 		Blinds blind = new Blinds();
 		AirCon airCon = new AirCon();
 		MainController mc = new MainController(timeSensor, tempSensor, blind, airCon);
 		
-		//new MainClass();
+		MainClass og = new MainClass(mc);
+		
+		while (thereisNoGod)
+		{
+			setAll(mc);
+		}
+		
 	}
+	
+	private static void setAll(MainController mc) {
+		// TODO Auto-generated method stub
+		int currentTemperature = getRandom(100);
+		int currentTime = mc.getCurrentTime();
+		
+		mc.set
+		
+		temp.setText("The AC Temperature is: ");	
+	}
+	
+	public static int getRandom(int max){
+        return (int) (Math.random()*max);
+    }
 
 }
